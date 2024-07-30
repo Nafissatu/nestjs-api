4ebe9098e537ef67930ad56b1297086077802a90
User <user@Users-MacBook-Pro.local>
2024-07-30 10:47:37 -0400

    added a new line on nest-cli.json

--- src/datasource/datasource.ts
+++ src/datasource/datasource.ts
@@ -4,7 +4,7 @@ dotenv.config();
 const dataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: 5433,
+  port: 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: 'postgres',
