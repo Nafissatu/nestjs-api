023ea3b3ff6d8d9103e27da8ea6807df93d6ac82
User <user@Users-MacBook-Pro.local>
2024-07-31 00:25:18 -0400

    fixed port number issue and added a new migration

--- src/migrations/1722399582524-add-todos.ts
+++ src/migrations/1722399582524-add-todos.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddTodosTable1721949342445 implements MigrationInterface {
-    name = 'AddTodosTable1721949342445'
+export class AddTodos1722399582524 implements MigrationInterface {
+    name = 'AddTodos1722399582524'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "todo" ("id" SERIAL NOT NULL, "description" character varying NOT NULL, "done" boolean NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_d429b7114371f6a35c5cb4776a7" PRIMARY KEY ("id"))`);
